5d7da8bfe469282b7935268fa5a8e965c2611804
Carolina Yang <carolinayang@Carolinas-MacBook-Pro.local>
2023-04-24 21:20:16 -0300

    Class

--- tests/test_math.py
+++ tests/test_math.py
@@ -14,3 +14,24 @@ def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError) as e:
         num = 1 / 0
     assert 'division by zero' in str(e.value)
+
+# MULTIPLICATION TEST IDEAS
+# two positive int
+# identity: multiplying any number by 1
+# zero: multiplying any number by 0
+# positive by a negative
+# negative by a negativ
+# multiply floats
+
+products = [
+    (2, 3, 6),
+    (1, 99, 99),
+    (0, 99, 0),
+    (3, -4, -12),
+    (-5, -5, 25),
+    (2.5, 6.7, 16.75)
+]
+
+@pytest.mark.parametrize('a, b, product', products)
+def test_multiplication(a, b, product):
+    assert a * b == product
