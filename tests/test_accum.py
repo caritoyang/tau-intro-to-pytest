a6d867b842f17bb971bacc4016a78ebbedf43900
CaroYang <cyang@totalperform.com>
2023-04-26 14:51:01 -0300

    Markers

--- tests/test_accum.py
+++ tests/test_accum.py
@@ -3,26 +3,38 @@ from stuff.accum import Accumulator
 
 ############## FIXTURE ##############
 @pytest.fixture
-def accum():
+def accum(scope="session"):
     return Accumulator()
 
+@pytest.fixture
+def accum2():
+    yield Accumulator() # generator
+    print("DONE-ZO!")
+
+
+
 ############## TESTS ##############
-def test_accumulator_init(accum):
+@pytest.mark.accumulator
+def test_accumulator_init(accum, accum2):
     assert accum.count == 0
 
+@pytest.mark.accumulator
 def test_accumulator_add_one(accum):
     accum.add()
     assert accum.count == 1
 
+@pytest.mark.accumulator
 def test_accumulator_add_three(accum):
     accum.add(3)
     assert accum.count == 3
 
+@pytest.mark.accumulator
 def test_accumulator_add_twice(accum):
     accum.add()
     accum.add()
     assert accum.count == 2
 
+@pytest.mark.accumulator
 def test_accumulator_cannot_set_count_directly(accum):
     with pytest.raises(AttributeError, match=r"has no setter") as e:
         accum.count = 10
