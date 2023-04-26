cc32c07c8f11bf234140a609921d7df5edd1bf24
CaroYang <cyang@totalperform.com>
2023-04-26 10:21:21 -0300

    Update fixture

--- tests/test_accum.py
+++ tests/test_accum.py
@@ -1,27 +1,28 @@
 import pytest
 from stuff.accum import Accumulator
 
-def test_accumulator_init():
-    accum = Accumulator()
+############## FIXTURE ##############
+@pytest.fixture
+def accum():
+    return Accumulator()
+
+############## TESTS ##############
+def test_accumulator_init(accum):
     assert accum.count == 0
 
-def test_accumulator_add_one():
-    accum = Accumulator
+def test_accumulator_add_one(accum):
     accum.add()
     assert accum.count == 1
 
-def test_accumulator_add_three():
-    accum = Accumulator
+def test_accumulator_add_three(accum):
     accum.add(3)
     assert accum.count == 3
 
-def test_accumulator_add_twice():
-    accum = Accumulator
+def test_accumulator_add_twice(accum):
     accum.add()
     accum.add()
     assert accum.count == 2
 
-def test_accumulator_cannot_Set_count_directly():
-    accum = Accumulator
-    with pytest.raises(AttributeError, match=r"can't set attribute") as e:
+def test_accumulator_cannot_set_count_directly(accum):
+    with pytest.raises(AttributeError, match=r"has no setter") as e:
         accum.count = 10
